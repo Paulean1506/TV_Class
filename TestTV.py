54f7b7dc5f13f2362c0b340ad0f2b1de7653232a
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-05-14 15:38:49 +0800

    Create second object named tv2

--- TestTV.py
+++ TestTV.py
@@ -1,8 +1,14 @@
 # Import the class
 from TV_Class import TV
 
-# create first TV object
+# Create the first TV object
 tv1 = TV()
 tv1.turnOn()
 tv1.setChannel(30)
 tv1.setVolume(3)
+
+# Create the Second TV object
+tv2 = TV()
+tv2.turnOn()
+tv2.setChannel(3)
+tv2.setVolume(2)
