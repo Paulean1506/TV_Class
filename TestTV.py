cbc952fe8d05f10829610454d5063f119ca7b809
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-05-14 15:58:33 +0800

    ask for user input to make it interactive

--- TestTV.py
+++ TestTV.py
@@ -1,17 +1,26 @@
 # Import the class
 from TV_Class import TV
 
+# Ask user for input
+Power=input(str("\nTo turn this TV on press 'y' "))
+print('\nThe current channel of this TV is (1-120)')
+print('The current volume level of this TV is (1-7)')
+Channel1=input("\nWhat channel for tv1 do you want? ")
+Volume1=input("What volume for tv1 do you prefer? ")
+Channel2=input("\nWhat channel for tv2 do you want? ")
+Volume2=input("What volume for tv2 do you prefer? ")
+
 # Create the first TV object
 tv1 = TV()
 tv1.turnOn()
-tv1.setChannel(30)
-tv1.setVolume(3)
+tv1.setChannel(Channel1)
+tv1.setVolume(Volume1)
 
 # Create the Second TV object
 tv2 = TV()
 tv2.turnOn()
-tv2.setChannel(3)
-tv2.setVolume(2)
+tv2.setChannel(Channel2)
+tv2.setVolume(Volume2)
 
 # Print the results
 print("tvl's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
